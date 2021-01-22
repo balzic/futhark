1525b993c8b3f4db50661e708953aee8f0ab3bf8
Philip Lassen <philiplassen+git@gmail.com>
2021-01-22 04:38:18 -0800

    Get Entry stuff from Monad

--- src/Futhark/Actions.hs
+++ src/Futhark/Actions.hs
@@ -23,11 +23,13 @@ import qualified Data.ByteString.Lazy.Char8 as ByteString
 import Data.Maybe (fromMaybe)
 import Futhark.Analysis.Alias
 import Futhark.Analysis.Metrics
+import Futhark.MonadFreshNames
 import qualified Futhark.CodeGen.Backends.CCUDA as CCUDA
 import qualified Futhark.CodeGen.Backends.COpenCL as COpenCL
 import qualified Futhark.CodeGen.Backends.MulticoreC as MulticoreC
 import qualified Futhark.CodeGen.Backends.SequentialC as SequentialC
 import qualified Futhark.CodeGen.ImpGen.Kernels as ImpGenKernels
+import qualified Futhark.CodeGen.ImpCode as ImpCode
 import qualified Futhark.CodeGen.ImpGen.Multicore as ImpGenMulticore
 import qualified Futhark.CodeGen.ImpGen.Sequential as ImpGenSequential
 import Futhark.Compiler.CLI
@@ -188,6 +190,12 @@ compileCAction fcfg mode outpath =
           liftIO $ writeFile cpath $ SequentialC.asExecutable cprog
           runCC cpath outpath ["-O3", "-std=c99"] ["-lm"]
 
+myCompile :: MonadFreshNames m => ImpCode.Definitions op -> m [(Name, ImpCode.FunctionT op)]
+myCompile xprog = 
+  let ImpCode.Definitions consts (ImpCode.Functions funs) = xprog
+      entry_funs = filter (ImpCode.functionEntry . snd) funs
+  in return entry_funs
+
 compileCtoWASMAction :: FutharkConfig -> CompilerMode -> FilePath -> Action SeqMem
 compileCtoWASMAction fcfg mode outpath =
   Action
@@ -203,7 +211,14 @@ compileCtoWASMAction fcfg mode outpath =
 
       case mode of
         ToLibrary -> do
+          jswrap <- handleWarnings fcfg $ (traverse myCompile <=< ImpGenSequential.compileProg) prog
           let (h, imp) = SequentialC.asLibrary cprog
+              --lassensImplementation xprog =
+              --    let ImpCode.Definitions consts (ImpCode.Functions funs) = xprog
+              --        entry_funs = filter (ImpCode.functionEntry . snd) funs
+              --    in entry_funs
+
+          -- Add something here that takes prog? and creates a javascript file
           liftIO $ writeFile hpath h
           liftIO $ writeFile cpath imp
         ToExecutable -> do
